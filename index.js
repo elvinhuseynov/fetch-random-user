abc87e7c4ae516cd5ca433ba13a7201398d82779
005676 <005676@ibar.int>
2025-02-12 10:23:36 +0400

    Fetch random user

---
+++ index.js
@@ -0,0 +1,16 @@
+// Bu tapşırıqda sizə artıq verilmiş HTML və CSS şablonuna 
+// yalnız JavaScript ilə fetch funksionallığını əlavə etmək qalıb. 
+// JavaScript-dən istifadə edərək, "https://randomuser.me/api/" ünvanından random istifadəçi məlumatlarını alıb, 
+// həmin məlumatları HTML-dəki təyin olunmuş elementlərə (şəkil, ad, email, telefon və s.) yerləşdirəcəksiniz. 
+// İstifadəçi "Yeni İstifadəçi" düyməsinə basdıqda isə, mövcud məlumatlar yenilənərək 
+// yeni random istifadəçi məlumatları göstəriləcək.
+
+// Bu təlimatı izləyərək bu tapşırığı daha rahat edə bilərsiniz:
+
+// Random istifadəçi məlumatlarını əldə edən funksiya yaradın.
+// Səhifə yüklənərkən ilkin random istifadəçi məlumatlarının alınması üçün bu funksiyanı birbaşa çağırın.
+// "Yeni İstifadəçi" düyməsinə kliklədikdə həmin funksiyanı yenidən çağırın.
+
+
+
+
